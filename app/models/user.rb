a93761c8aa89f878ad17a27fd7daf90e39b44667
KDS <ds.krav@gmail.com>
2019-03-29 21:13:28 +0800

    add passed tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.where(level: level).joins('INNER JOIN PassedTest ON tests.id = PassedTest.test_id').where(user_tests: {user_id: self.id})
+    Test.where(level: level).joins('INNER JOIN passed_tests ON tests.id = passed_tests.test_id').where(passed_tests: {user_id: self.id})
   end
 end
