fd6ee7f109a8afa04816e26f040d4525e89a61a6
KDS <ds.krav@gmail.com>
2019-03-29 22:19:39 +0800

    add passed tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
-  def self.tests_by_category(category)
-    Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: {body: category}).order('tests.name DESC').pluck('tests.name')
+  def self.get_tests_by_category(category)
+    Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
+        .where(categories: {body: category})
+        .order('tests.name DESC')
+        .pluck('tests.name')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  def tests_by_level(level)
-    Test.where(level: level).joins('INNER JOIN passed_tests ON tests.id = passed_tests.test_id').where(passed_tests: {user_id: self.id})
+  def get_tests_by_level(level)
+    Test.where(level: level)
+        .joins('INNER JOIN passed_tests ON tests.id = passed_tests.test_id')
+        .where(passed_tests: {user_id: self.id})
   end
 end
