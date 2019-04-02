cc870419cf52da5c60aba4539b43a3457df0e545
KDS <ds.krav@gmail.com>
2019-04-02 13:43:59 +0800

    added connections between models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions
+  belongs_to :category
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
+  belongs_to :author, class_name: 'User', optional: true
+
   def self.by_category(category)
     joins('INNER JOIN categories ON tests.category_id = categories.id', categories: { title: category })
       .order(name: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  #has_and_belongs_to_many :tests
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
+
   def tests_by_level(level)
     where(level: level, passed_tests: {user_id: self.id})
         .joins('INNER JOIN passed_tests ON tests.id = passed_tests.test_id')
