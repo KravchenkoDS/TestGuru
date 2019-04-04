491dbbe714cb75d110946872983fcf7fe3ae584a
KDS <ds.krav@gmail.com>
2019-04-04 11:49:33 +0800

    error correction

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
-  has_many :questions
   belongs_to :category
-  has_many :passed_tests
-  has_many :users, through: :passed_tests
   belongs_to :author, class_name: 'User', optional: true
+  has_many :questions, dependent: :destroy
+  has_many :passed_tests, dependent: :destroy
+  has_many :users, through: :passed_tests, dependent: :destroy
 
   def self.by_category(category)
-    joins('INNER JOIN categories ON tests.category_id = categories.id', categories: { title: category })
+    joins('INNER JOIN categories ON tests.category = categories', categories: { title: category })
       .order(name: :desc)
         .pluck(:name)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  #has_and_belongs_to_many :tests
-  has_many :passed_tests
-  has_many :tests, through: :passed_tests
+  has_many :passed_tests, dependent: :destroy
+  has_many :tests, through: :passed_tests, dependent: :destroy
 
   def tests_by_level(level)
     where(level: level, passed_tests: {user_id: self.id})
