cd572d3045537a1c37383886052cdf7be797d34a
KDS <ds.krav@gmail.com>
2019-04-09 13:40:12 +0800

    error correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ApplicationRecord
+  has_many :author, class_name: 'Test', foreign_key: :id
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests, dependent: :destroy
 
-=begin
-  def tests_by_level(level)
-    where(level: level, passed_tests: {user_id: self.id})
-        .joins('INNER JOIN passed_tests ON tests.id = passed_tests.test_id')
-  end
-=end
   def test_by_level(level)
     tests.where(level: level)
   end
