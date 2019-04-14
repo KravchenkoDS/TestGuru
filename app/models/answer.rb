13a0bd0e1039aa630220988f8f7257266f8b3658
KDS <ds.krav@gmail.com>
2019-04-14 16:24:47 +0800

    rework on remarks

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,10 +4,10 @@ class Answer < ApplicationRecord
 
   belongs_to :question
 
-  scope :correct, -> {where(correct: true)}
-
   validates :body, presence: true
   validate :validate_max_answers, on: :create
+
+  scope :correct, -> {where(correct: true)}
 
   def validate_max_answers
     errors.add(:base, 'Must have 1 to 4 answers') if question.answers.count >= MAX_ANSWERS_NUMBER
