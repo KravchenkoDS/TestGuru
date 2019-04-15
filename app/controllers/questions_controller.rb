e49814857f5b872da3c105f147aa572f3ca7dccc
KDS <ds.krav@gmail.com>
2019-04-16 01:10:27 +0800

    add controllers and wiews

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,17 @@
 class QuestionsController < ApplicationController
-  before_action :find_test_by_id, only: %i[index create new]
-  before_action :find_question_by_id, only: %i[show destroy]
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_question_not_found
 
-  def show
-  end
+  def show; end
 
   def index
     @question = @test.questions
     render inline: '<% @question.each { |q| %><p><%= q.body %></p><% } %>'
   end
 
-  def new
-  end
+  def new; end
 
   def create
     @test.questions.create(params_question)
@@ -27,11 +25,11 @@ class QuestionsController < ApplicationController
 
   private
 
-  def find_test_by_id
+  def find_test
     @test = Test.find(params[:test_id])
   end
 
-  def find_question_by_id
+  def find_question
     @question = Question.find(params[:id])
   end
 
