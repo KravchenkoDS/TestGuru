0b8055293c7bc9999bc32bd615aa618028b8c40a
KDS <ds.krav@gmail.com>
2019-04-18 11:47:21 +0800

    add rail views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,31 +1,32 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create new]
-  before_action :find_question, only: %i[show destroy edit update]
+  before_action :set_test, only: [:new, :create]
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_question_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def show; end
+  def show
+  end
 
   def new
     @question = @test.questions.new
-  end
-
-  def edit;
   end
 
   def create
     @question = @test.questions.new(question_params)
 
     if @question.save
-      redirect_to @test
+      redirect_to test_path(@test)
     else
       render :new
     end
   end
 
+  def edit
+  end
+
   def update
     if @question.update(question_params)
-      redirect_to @question
+      redirect_to test_path(@question.test)
     else
       render :edit
     end
@@ -33,24 +34,24 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    render inline: '<h1>Deleted</h1>'
+    redirect_to test_path(@question.test)
   end
 
   private
 
-  def rescue_from_question_not_found
-    render plain: 'Request not found'
-  end
-
-  def find_test
+  def set_test
     @test = Test.find(params[:test_id])
   end
 
-  def find_question
+  def set_question
     @question = Question.find(params[:id])
   end
 
   def question_params
-    params.require(:question).permit(:body)
+    params.require(:question).permit(:body, :test_id)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,22 @@
+=begin
 module QuestionsHelper
   def question_header(question)
     header = question.persisted? ? "Edit " : "Create new "
     header + "#{question.test.name} #{level_test(question.test)} test question."
+  end
+end
+=end
+
+module QuestionsHelper
+  def question_header(question)
+    test_name = question.test.name
+
+    name = if question.new_record?
+             "Create New #{test_name} Question"
+           else
+             "Edit #{test_name} Question"
+           end
+
+    content_tag('h1', name)
   end
 end
