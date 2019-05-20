076173653bd1168dc584d8efda4dff1c6b641b6c
KDS <ds.krav@gmail.com>
2019-05-20 20:05:29 +0800

    add localizations

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,6 @@
 class User::SessionsController < Devise::SessionsController
   def create
     super
-    flash[:notice] = "Hello, #{current_user.full_name.html_safe}!"
+    flash[:notice] = "#{t('.hello')}, #{current_user.full_name.html_safe}!"
   end
 end
