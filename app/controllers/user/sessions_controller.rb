90efc99e2303fff95e0131872c54f783b0953b41
KDS <ds.krav@gmail.com>
2019-05-21 11:08:38 +0800

    completion of comments

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,6 @@
 class User::SessionsController < Devise::SessionsController
   def create
     super
-    flash[:notice] = "#{t('.hello')}, #{current_user.full_name.html_safe}!"
+    flash[:notice] = t('.hello', user_name: current_user.full_name.html_safe)
   end
 end
