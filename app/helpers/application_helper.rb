4413190786a0a676aef3829c22ed6e9061f06c66
KDS <ds.krav@gmail.com>
2019-05-22 23:04:26 +0800

    исправил flash_message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,18 @@
 module ApplicationHelper
+
+  BOOTSTRAP_ALERTS = { 'notice' => 'alert-success', 'alert' => 'alert-danger' }.freeze
+  DEFAULT_BOOTSTRAP_ALERT = 'alert-primary'.freeze
+
+  def flash_message(type, msg)
+    alert_type = BOOTSTRAP_ALERTS[type] || DEFAULT_BOOTSTRAP_ALERT
+    content_tag :div, msg, class: "alert #{alert_type}"
+  end
+
   def current_year
     Time.current.year
   end
 
   def github_url(author, repo)
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
-  end
-
-  def flash_message(type, msg)
-    content_tag :p, msg, class: "flash #{type}"
   end
 end
