c3b784e8de4c26e6f36502a2610d850900c04b46
KDS <ds.krav@gmail.com>
2019-06-03 15:28:24 +0800

    commit for debug heroku

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,5 @@
 class ContactsController < ApplicationController
-
   skip_before_action :authenticate_user!, only: [:new, :create]
-  #before_action :authenticate_user!
 
   def new
     @contact = Contact.new
